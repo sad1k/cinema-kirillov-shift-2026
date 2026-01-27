ace6c5f64fef62a6547a9066041b969656e0f155
mikhail.kirillov <misha.kirillov.0990@gmail.com>
2026-01-27 14:37:50 +0300

    :sparkles: chore: add eslint antfu

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,19 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import antfu from "@antfu/eslint-config";
 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
-]);
+export default antfu({
+  nextjs: true,
+  typescript: true,
 
-export default eslintConfig;
+  lessOpinionated: true,
+
+  stylistic: {
+    indent: 2,
+    quotes: "single",
+  },
+
+  formatters: {
+    css: true,
+  },
+
+  ignores: ["next-env.d.ts"],
+});
